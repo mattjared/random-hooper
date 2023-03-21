22e5cafe7ccb8bdd7565819c27f1bb4e0e10b3bd
Matt Jared <matt.jared@vercel.com>
2023-03-21 18:48:57 -0500

    committing

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,11 @@
+import { Redis } from '@upstash/redis'
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  const member = await redis.srandmember<string>("nextjs13")
+  return new Response(member);
 }
