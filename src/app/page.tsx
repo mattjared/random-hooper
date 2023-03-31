145e72249c79ecb3ecfcbb0c7d3f0c409cfaa461
Matt Jared <matt.jared@vercel.com>
2023-03-30 20:25:17 -0500

    committing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
-import { Redis } from '@upstash/redis'
+// import { Redis } from '@upstash/redis'
 import styles from './page.module.css'
 
-export const revalidate = 0 // disable cache
+// export const revalidate = 0 // disable cache
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-	token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+// const redis = new Redis({
+//   url: process.env.UPSTASH_REDIS_REST_URL,
+// 	token: process.env.UPSTASH_REDIS_REST_TOKEN,
+// })
 
 export default async function Home() {
-  const member = await redis.srandmember<string>("hoopers") 
-  console.log(member); 
+  // const member = await redis.srandmember<string>("hoopers") 
+  // console.log(member); 
 
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome {member}
+          Welcome {"member"}
         </h1>
         <p className={styles.description}>
           You have been randomly chosen by the redis algorithm.
